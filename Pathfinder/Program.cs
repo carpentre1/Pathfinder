54cb3089b8fb804b32972b0d92b5fe5abaeaa38b
carpentre1 <jlowell.carpenter@gmail.com>
2020-01-29 14:38:29 -0600

    Created C# project
    added comments describing assignment

---
+++ Pathfinder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+int array, values > 0 == impassable
+in the array and value you want indicating impassable
+starting position in x,y
+destination in x,y
+GOAL
+    Ageny myAgent = newAgent(arrayOfLocales, startingPos, endingPos);
+    var solution = newAgent.SolvePath();
+where solution == a set of x,y coordinates, for each move,
+from the start to the end that doesn't pass through impassable AND is the most efficient path you can resolve
+only one unit (grid) move per "turn".
+*/
+
+namespace Pathfinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
